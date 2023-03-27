0880ee91f587caec5bfdb1ed343cfdd7312d818b
ChinmayeePani <cpani1997@gmail.com>
2023-03-27 10:06:51 +0530

    Card_App

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      {/* 1 */}
+      <div className="box">
+        <h3>Cards</h3>
+        <p>
+          “I never could have done what I have done, without the habits of punctuality, order, and
+          diligence, without the determination to concentrate myself on one object at a time.”
+        </p>
+        <a href="#"><button>Click Here</button></a>
+        <span className="count">1</span>
+      </div>
+
+      {/* 2 */}
+      <div className="box">
+        <h3>Cards</h3>
+        <p>
+          “I never could have done what I have done, without the habits of punctuality, order, and
+          diligence, without the determination to concentrate myself on one object at a time.”
+        </p>
+        <a href="#"><button>Click Here</button></a>
+        <span className="count">2</span>
+        </div>
+
+        {/* /* 3 */}
+        <div className="box">
+        <h3>Cards</h3>
+        <p>
+          “I never could have done what I have done, without the habits of punctuality, order, and
+          diligence, without the determination to concentrate myself on one object at a time.”
+        </p>
+        <a href="#"><button>Click Here</button></a>
+        <span className="count">3</span>
+        </div>
+
+
+  
+    </div>
+  );
+}
+
+export default App;
